4bd087b0c19f91ded87491b83a9f04bb91077b35
Peter Zweifel <71866336+networkscientist@users.noreply.github.com>
2022-10-05 20:41:55 +0200

    feature: Bash script to merge netCDF files
    Wrote a Bash script that merges netCDF forcing files of a range of years into a single file to be used in RAVEN

---
+++ raven_tools/nc_combine.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#### Script that merges netCDF files of different years into 1 file.
+source nc_combine_config.txt # Reads in the FORCING_DIR value from a config file, which is the forcing files directory.
+RAIN_DIR="${FORCING_DIR}RhiresD_v2.0_swiss.lv95/"
+RAIN_FILE="RhiresD_ch01h.swiss.lv95_"
+T_MEAN_FILE="TabsD_ch01r.swiss.lv95_"
+T_MAX_FILE="TmaxD_ch01r.swiss.lv95_"
+T_MIN_FILE="TminD_ch01r.swiss.lv95_"
+T_MEAN_DIR="${FORCING_DIR}TabsD_v2.0_swiss.lv95/"
+T_MAX_DIR="${FORCING_DIR}TmaxD_v2.0_swiss.lv95/"
+T_MIN_DIR="${FORCING_DIR}TminD_v2.0_swiss.lv95/"
+
+DIRS=($RAIN_DIR $T_MEAN_DIR $T_MAX_DIR $T_MIN_DIR)
+FILES=($RAIN_FILE $T_MEAN_FILE $T_MAX_FILE $T_MIN_FILE)
+
+#### merge_netcdf BEGIN
+# Merges netCDF files of different years into one file.
+# GLOBALS:
+#        FORCING_DIR
+#        RAIN_DIR
+#        RAIN_FILE
+#        T_MEAN_FILE
+#        T_MAX_FILE
+#        T_MIN_FILE
+#        T_MEAN_DIR
+#        T_MAX_DIR
+#        T_MIN_DIR
+# ARGUMENTS:
+#        Start year as an integer
+#        End year as an integer
+# OUTPUTS:
+#        Merged netCDF files
+# Return:
+#        0 if success, non-zero otherwise.
+### FUNCTION END
+function merge_netcdf(){
+  # Take in the start and end year as arguments
+  start_date=$1
+  end_date=$2
+  # Create empty arrays
+  declare -a DATE_RANGE
+  declare -a IN_PATHS
+  # For loop that iterates over the years
+  for i in $(seq $start_date $end_date)
+  do
+    # Appends the year to the DATE_RANGE array
+    DATE_RANGE+=("$i")
+    # Appends created filename to the input file paths array IN_PATHS
+    IN_PATHS+=("${DIRS[0]}out/RhiresD_ch01h.swiss.lv95_${i}01010000_${i}12310000_clipped.nc")
+  done
+  # Create a counter to use in the next for loop
+  COUNTER=0
+  # Iterates over each directory in the DIRS array
+  for i in "${DIRS[@]}"
+  do
+    # uses the cdo command and expands the IN_PATHS to use every value as arguments
+    cdo -mergetime "${IN_PATHS[@]}" "${i}merged/${FILES[$COUNTER]}${DATE_RANGE[0]}01010000_${DATE_RANGE[-1]}12310000_clipped.nc"
+    ((COUNTER=COUNTER+1))
+  done
+}
+
+merge_netcdf $1 $2
+
