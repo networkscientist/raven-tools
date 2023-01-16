3782eb1cccebf200591a3176bb81cd0757eacd92
Peter Zweifel <71866336+networkscientist@users.noreply.github.com>
2023-01-16 16:30:18 +0100

    refactor: Grid weight generation now implemented in raven_preprocess.py

--- raven_tools/raven_preprocess.py
+++ raven_tools/raven_preprocess.py
@@ -243,7 +243,29 @@ def nc_merge(start_year: int, end_year: int, forcing_dir: str):
     subprocess.call(['raven_tools/nc_combine.sh', str(start_year), str(end_year), forcing_dir])
 
 
-def create_grid(export_shp = True):
+def create_grid(netcdf_filepath: Path, bounding_box_filename: Path, export_shp=True):
+
+    # Read in the bounding box shape file from the clipping folder as a GeoPandas DataFrame
+    bbox: GeoDataFrame = gpd.read_file(bounding_box_filename)
+    bbox.set_crs(21781, allow_override=True)
+    bbox = bbox.to_crs("EPSG:2056")
+
+    # Read in the clipped netCDF file into a xarray Dataset
+    ds: Dataset = xr.open_dataset(netcdf_filepath, engine="netcdf4")
+
+    # Since we're only interested in the grid (not the actual cell values), only use 1 day
+    ds = ds.sel(time=slice('1962-01-01', '1962-01-02'))
+    # Select the actual data value column
+    xarr: xr.DataArray = ds['RhiresD']
+    # Convert Dataset to DataFrame and reset the index
+    df: pd.DataFrame = xarr.to_dataframe().reset_index()
+    # Convert the DataFrame to a GeoDataFrame, using the northing and easting provided by the netCDF. Furthermore,
+    # change the projection to LV95
+    combi_catchment_grid: GeoDataFrame = gpd.GeoDataFrame(df.RhiresD, geometry=gpd.points_from_xy(df.E, df.N),
+                                                          crs="EPSG:2056")
+
+    # From the GeoDataFrame that contains the netCDF grid, get the extent as points
+    xmin, ymin, xmax, ymax = combi_catchment_grid.total_bounds
     # Since the netCDF has a cell size of 1000m, set this here
     length: int = 1000
     wide: int = 1000
@@ -274,6 +296,7 @@ def create_grid(export_shp = True):
     # Every cell that is not within the catchment area will have area_rel set to zero
     grid["area_rel"] = 0
     grid = grid.set_crs(2056)
+    return grid
 
     if export_shp:
         # Export the grid to a shape file
@@ -363,7 +386,7 @@ def write_weights_to_file(grd: GeoDataFrame, filename: Path):
         ff.write('   #                                \n')
         ff.write('   # [# HRUs]                       \n')
         ff.write('   :NumberHRUs       {0}            \n'.format(1))  # Currently for GR4J, there's 1 HRU
-        ff.write('   :NumberGridCells  {0}            \n'.format(len(grid.index)))
+        ff.write('   :NumberGridCells  {0}            \n'.format(len(grd.index)))
         ff.write('   #                                \n')
         ff.write('   # [HRU ID] [Cell #] [w_kl]       \n')
         for idata in data:
@@ -420,95 +443,6 @@ def copy_rel_area_from_union_to_grid(uni: GeoDataFrame, grd: GeoDataFrame):
         grd.at[ind[0], "area_rel"] = area_rel_old_value
     return grd
 
+
 if __name__ == '__main__':
-    home_path = Path.home()
-    output_file = Path(
-        "/media/mainman/Data/RAVEN/data/MeteoSwiss_gridded_products/RhiresD_v2.0_swiss.lv95/out/GridWeights.txt")
-    bounding_box_filename = Path(
-        "/media/mainman/Data/RAVEN/data/MeteoSwiss_gridded_products/RhiresD_v2.0_swiss.lv95/out/bbox.shp")
-    # The shape file that defines the extent of the catchment
-    extent_shape_file_path = Path("/media/mainman/Data/RAVEN/data/Catchment/Broye_Payerne.shp")
-    # The netCDF file that will be used to get the bounds/extent
-    netCDF_file_path = Path(
-        "/media/mainman/Data/RAVEN/data/MeteoSwiss_gridded_products/RhiresD_v2.0_swiss.lv95/out"
-        "/RhiresD_ch01h.swiss.lv95_196201010000_196212310000_clipped.nc")
-
-    # Read in the bounding box shape file from the clipping folder as a GeoPandas DataFrame
-    bbox: GeoDataFrame = gpd.read_file(bounding_box_filename)
-    bbox.set_crs(21781, allow_override=True)
-    bbox = bbox.to_crs("EPSG:2056")
-
-    # Read in the clipped netCDF file into a xarray Dataset
-    ds: Dataset = xr.open_dataset(netCDF_file_path, engine="netcdf4")
-
-    # Since we're only interested in the grid (not the actual cell values), only use 1 day
-    ds = ds.sel(time=slice('1962-01-01', '1962-01-02'))
-    # Select the actual data value column
-    xarr: xr.DataArray | xr.Dataset = ds['RhiresD']
-    # Convert Dataset to DataFrame and reset the index
-    df: pd.DataFrame = xarr.to_dataframe().reset_index()
-    # Convert the DataFrame to a GeoDataFrame, using the northing and easting provided by the netCDF. Furthermore,
-    # change the projection to LV95
-    combi_catchment_grid: GeoDataFrame = gpd.GeoDataFrame(df.RhiresD, geometry=gpd.points_from_xy(df.E, df.N),
-                                                          crs="EPSG:2056")
-
-    # From the GeoDataFrame that contains the netCDF grid, get the extent as points
-    xmin, ymin, xmax, ymax = combi_catchment_grid.total_bounds
-
-    # Since the netCDF has a cell size of 1000m, set this here
-    length: int = 1000
-    wide: int = 1000
-
-    # Create the northing and easting coordinates of the bounding vertex for each cell
-    cols: list = list(np.arange(xmin, xmax + wide, wide))
-    rows: list = list(np.arange(ymin, ymax + length, length))
-
-    # initialize the Polygon list
-    polygons: list[Polygon] = []
-    cell_id: list[str] = []
-
-    # Create the GeoDataFrame with the grid, providing column names plus polygons for the geometry
-    grid_cols = ['row', 'col', 'cell_id', 'polygons', 'area', 'area_rel']
-    grid: GeoDataFrame = gpd.GeoDataFrame(columns=grid_cols, geometry='polygons')
-    # Loop over each cell and create the corresponding Polygon, then append it to the Polygon list
-    # Also write the cell id from cell_id=i_row*n_columns + i_column
-    for ix, x in enumerate(cols):
-        for iy, y in enumerate(rows):
-            polygons.append(Polygon([(x, y), (x + wide, y), (x + wide, y + length), (x, y + length)]))
-            cid = int(iy * len(cols) + ix)
-            cell_id.append(f"{str(cid)}")
-
-    # Use the polygon list in the GeoDataFrame and set the projection accordingly
-    grid["polygons"] = polygons
-    grid["cell_id"] = cell_id
-
-    # Every cell that is not within the catchment area will have area_rel set to zero
-    grid["area_rel"] = 0
-    grid = grid.set_crs(2056)
-
-    # Export the grid to a shape file
-    grid.to_file("/media/mainman/Data/RAVEN/data/MeteoSwiss_gridded_products/grid.shp")
-
-    # Read the catchment shape file into a GeoDataFrame and set the projection accordingly
-    catchment = gpd.read_file(extent_shape_file_path)
-    catchment.set_crs(2056)
-
-    # Combine the features of the catchment and grid layers into one new GeoDataFrame (so a union can take place) and set
-    # the projection accordingly.
-    combi_catchment_grid = gpd.GeoDataFrame(pd.concat([catchment, grid]))
-    combi_catchment_grid.set_crs(2056)
-
-    # Create union and difference overlay GeoDataFrames
-    res_union, res_diff = create_overlay(grid, catchment)
-
-    # Compute the relative area a.k.a grid weight and write to shape files
-    res_union = calc_relative_area(res_union)
-    res_union.to_file("/media/mainman/Data/RAVEN/data/union.shp")
-    res_diff = calc_relative_area(res_diff)
-    res_diff.to_file("/media/mainman/Data/RAVEN/data/difference.shp")
-    grid.set_index("cell_id")
-
-    # Write a RAVEN compatible grid weights file
-    copy_rel_area_from_union_to_grid(res_union, grid)
-    write_weights_to_file(grid, output_file)
-    print()
+    pass
