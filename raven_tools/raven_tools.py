549cadbb43939efa8be7100da051e1927505cff2
networkscientist <71866336+networkscientist@users.noreply.github.com>
2023-01-19 19:26:30 +0100

    refactor: Commented out parts imported from create_grid_weight.py
    There's a bunch of code that has been imported from the grid weights generation script. So as not to run it, it's now commented out.

--- raven_tools/raven_tools.py
+++ raven_tools/raven_tools.py
@@ -19,53 +19,53 @@ for m in models:
         model_instance.write_rvx(ostrich_template=True, rvx_type="rvt")
     model_instance.write_ost()
 
-import os
-src = '/home/mainman/PycharmProjects/raven-tools/raven_tools/preappend_dblquotes.py'
-dst = '/home/mainman/PycharmProjects/raven-tools/raven_tools/preappend_dblquotes_symlink.py'
-os.symlink(src, dst)
+# import os
+# src = '/home/mainman/PycharmProjects/raven-tools/raven_tools/preappend_dblquotes.py'
+# dst = '/home/mainman/PycharmProjects/raven-tools/raven_tools/preappend_dblquotes_symlink.py'
+# os.symlink(src, dst)
 
-import raven_tools.raven_preprocess as rpe
-from pathlib import Path
-import geopandas as gpd
-import pandas as pd
+# import raven_tools.raven_preprocess as rpe
+# from pathlib import Path
+# import geopandas as gpd
+# import pandas as pd
 
-home_path = Path.home()
-netCDF_file_path = Path(
-        "/media/mainman/Data/RAVEN/data/MeteoSwiss_gridded_products/RhiresD_v2.0_swiss.lv95/out"
-        "/RhiresD_ch01h.swiss.lv95_196201010000_196212310000_clipped.nc")
-bounding_box_filename = Path(
-        "/media/mainman/Data/RAVEN/data/MeteoSwiss_gridded_products/RhiresD_v2.0_swiss.lv95/out/bbox.shp")
-output_file = Path(
-    "/media/mainman/Data/RAVEN/data/MeteoSwiss_gridded_products/RhiresD_v2.0_swiss.lv95/out/GridWeights.txt")
-bounding_box_filename = Path(
-    "/media/mainman/Data/RAVEN/data/MeteoSwiss_gridded_products/RhiresD_v2.0_swiss.lv95/out/bbox.shp")
-# The shape file that defines the extent of the catchment
-extent_shape_file_path = Path("/media/mainman/Data/RAVEN/data/Catchment/Broye_Payerne.shp")
-# The netCDF file that will be used to get the bounds/extent
-netCDF_file_path = Path(
-    "/media/mainman/Data/RAVEN/data/MeteoSwiss_gridded_products/RhiresD_v2.0_swiss.lv95/out"
-    "/RhiresD_ch01h.swiss.lv95_196201010000_196212310000_clipped.nc")
-
-grid = rpe.create_grid(netCDF_file_path, bounding_box_filename, export_shp=False)
-# Read the catchment shape file into a GeoDataFrame and set the projection accordingly
-catchment = gpd.read_file(extent_shape_file_path)
-catchment.set_crs(2056)
-
-# Combine the features of the catchment and grid layers into one new GeoDataFrame (so a union can take place) and set
-# the projection accordingly.
-combi_catchment_grid = gpd.GeoDataFrame(pd.concat([catchment, grid]))
-combi_catchment_grid.set_crs(2056)
-
-# Create union and difference overlay GeoDataFrames
-res_union, res_diff = rpe.create_overlay(grid, catchment)
-
-# Compute the relative area a.k.a grid weight and write to shape files
-res_union = rpe.calc_relative_area(res_union)
-res_union.to_file("/media/mainman/Data/RAVEN/data/union.shp")
-res_diff = rpe.calc_relative_area(res_diff)
-res_diff.to_file("/media/mainman/Data/RAVEN/data/difference.shp")
-grid.set_index("cell_id")
-
-# Write a RAVEN compatible grid weights file
-grid: gpd.GeoDataFrame = rpe.copy_rel_area_from_union_to_grid(res_union, grid)
-rpe.write_weights_to_file(grid, output_file)
+# home_path = Path.home()
+# netCDF_file_path = Path(
+#         "/media/mainman/Data/RAVEN/data/MeteoSwiss_gridded_products/RhiresD_v2.0_swiss.lv95/out"
+#         "/RhiresD_ch01h.swiss.lv95_196201010000_196212310000_clipped.nc")
+# bounding_box_filename = Path(
+#         "/media/mainman/Data/RAVEN/data/MeteoSwiss_gridded_products/RhiresD_v2.0_swiss.lv95/out/bbox.shp")
+# output_file = Path(
+#     "/media/mainman/Data/RAVEN/data/MeteoSwiss_gridded_products/RhiresD_v2.0_swiss.lv95/out/GridWeights.txt")
+# bounding_box_filename = Path(
+#     "/media/mainman/Data/RAVEN/data/MeteoSwiss_gridded_products/RhiresD_v2.0_swiss.lv95/out/bbox.shp")
+# # The shape file that defines the extent of the catchment
+# extent_shape_file_path = Path("/media/mainman/Data/RAVEN/data/Catchment/Broye_Payerne.shp")
+# # The netCDF file that will be used to get the bounds/extent
+# netCDF_file_path = Path(
+#     "/media/mainman/Data/RAVEN/data/MeteoSwiss_gridded_products/RhiresD_v2.0_swiss.lv95/out"
+#     "/RhiresD_ch01h.swiss.lv95_196201010000_196212310000_clipped.nc")
+#
+# grid = rpe.create_grid(netCDF_file_path, bounding_box_filename, export_shp=False)
+# # Read the catchment shape file into a GeoDataFrame and set the projection accordingly
+# catchment = gpd.read_file(extent_shape_file_path)
+# catchment.set_crs(2056)
+#
+# # Combine the features of the catchment and grid layers into one new GeoDataFrame (so a union can take place) and set
+# # the projection accordingly.
+# combi_catchment_grid = gpd.GeoDataFrame(pd.concat([catchment, grid]))
+# combi_catchment_grid.set_crs(2056)
+#
+# # Create union and difference overlay GeoDataFrames
+# res_union, res_diff = rpe.create_overlay(grid, catchment)
+#
+# # Compute the relative area a.k.a grid weight and write to shape files
+# res_union = rpe.calc_relative_area(res_union)
+# res_union.to_file("/media/mainman/Data/RAVEN/data/union.shp")
+# res_diff = rpe.calc_relative_area(res_diff)
+# res_diff.to_file("/media/mainman/Data/RAVEN/data/difference.shp")
+# grid.set_index("cell_id")
+#
+# # Write a RAVEN compatible grid weights file
+# grid: gpd.GeoDataFrame = rpe.copy_rel_area_from_union_to_grid(res_union, grid)
+# rpe.write_weights_to_file(grid, output_file)
