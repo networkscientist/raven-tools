607268146a8a06360a9e8a70970e47c22146e2fa
Peter Zweifel <71866336+networkscientist@users.noreply.github.com>
2023-01-21 20:34:42 +0100

    Update preappend_dblquotes.py

--- raven_tools/preappend_dblquotes.py
+++ raven_tools/preappend_dblquotes.py
@@ -1,4 +1,4 @@
-file1 = open('RAVEN/testmodels/Broye/HYMOD/Ost-RAVEN.sh','r')
-Lines = file1.readlines()
-for line in Lines:
-    print(f"f\"{line.strip()}\",")
+# file1 = open('RAVEN/testmodels/Broye/HYMOD/Ost-RAVEN.sh','r')
+# Lines = file1.readlines()
+# for line in Lines:
+#     print(f"f\"{line.strip()}\",")
